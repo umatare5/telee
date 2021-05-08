150e51c7119987ba867645c62a81b00d0bcaf206
umatare5 <umatare5@gmail.com>
2021-05-09 04:38:57 +0900

    Provide ScreenOSUsecase with HA

--- internal/application/usecases/screenos/usecase.go
+++ internal/application/usecases/screenos/usecase.go
@@ -7,15 +7,28 @@ import (
 	x "github.com/google/goexpect"
 )
 
+const (
+	noSuffix string = ""
+	haSuffix string = "\\(M\\)"
+)
+
 // Usecase struct
 type Usecase struct {
 	Config     *config.Config
 	Repository *infrastructure.Repository
+	HAMode     bool
 }
 
 // Fetch returns stdout from telnet session
 func (u *Usecase) Fetch() (string, error) {
-	expection := u.buildRequest()
+	var expection []x.Batcher
+
+	if u.HAMode {
+		expection = u.buildRequest(haSuffix)
+	} else {
+		expection = u.buildRequest(noSuffix)
+	}
+
 	data, err := u.Repository.InvokeServerRepository().Fetch(&expection)
 	if err != nil {
 		return "", err
@@ -24,16 +37,16 @@ func (u *Usecase) Fetch() (string, error) {
 }
 
 // [platform: ssg] buildRequest returns the expection
-func (u *Usecase) buildRequest() []x.Batcher {
+func (u *Usecase) buildRequest(suffix string) []x.Batcher {
 	return []x.Batcher{
 		&x.BExp{R: "login:"},
 		&x.BSnd{S: u.Config.Username + "\n"},
 		&x.BExp{R: "password:"},
 		&x.BSnd{S: u.Config.Password + "\n"},
-		&x.BExp{R: u.Config.Hostname + "\\(M\\)->"},
+		&x.BExp{R: u.Config.Hostname + suffix + "->"},
 		&x.BSnd{S: "set console page 0\n"},
-		&x.BExp{R: u.Config.Hostname + "\\(M\\)->"},
+		&x.BExp{R: u.Config.Hostname + suffix + "->"},
 		&x.BSnd{S: u.Config.Command + "\n"},
-		&x.BExp{R: u.Config.Hostname + "\\(M\\)->"},
+		&x.BExp{R: u.Config.Hostname + suffix + "->"},
 	}
 }
