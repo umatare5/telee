b2eb88de2d69eab74e4ab8b867e84956a6395730
umatare5 <umatare5@gmail.com>
2021-05-13 03:10:02 +0900

    Bump version to v1.3.11

--- cli/root.go
+++ cli/root.go
@@ -16,7 +16,7 @@ const (
 	cmdName      string = "telee"
 	cmdUsage     string = "One-line telnet client"
 	cmdUsageText string = "telee -H HOSTNAME -C COMMAND [options...]"
-	cmdVersion   string = "1.3.10"
+	cmdVersion   string = "1.3.11"
 )
 
 // Start executes this command
