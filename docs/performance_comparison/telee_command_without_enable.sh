4b63b32cae6e6d38818f6655e16a63e9c8d65346
umatare5 <umatare5@gmail.com>
2021-05-15 08:02:13 +0900

    Update performance comparison

--- docs/performance_comparison/telee_command_without_enable.sh
+++ docs/performance_comparison/telee_command_without_enable.sh
@@ -8,7 +8,7 @@ TELEE_COMMAND=$1
 
 for _ in $(seq "$LOOP_START" "$LOOP_END"); \
 do \
-  time telee --hostname $TELEE_HOSTNAME --command "$TELEE_COMMAND" --enable > /dev/null
+  time telee --hostname $TELEE_HOSTNAME --command "$TELEE_COMMAND" > /dev/null
   sleep 3
 done
 
