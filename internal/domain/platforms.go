b2556c3bf05de43d13568b43a2fe57d8c971a689
umatare5 <umatare5@gmail.com>
2021-05-15 02:39:16 +0900

    Sort constants alphabetically

--- internal/domain/platforms.go
+++ internal/domain/platforms.go
@@ -2,13 +2,13 @@ package domain
 
 // Used for routing in usecases
 const (
-	IOSPlatformName         string = "ios"
-	NXOSPlatformName        string = "nxos"
-	IronWarePlatformName    string = "foundry"
 	AireOSPlatformName      string = "aireos"
 	AlliedWarePlatformName  string = "allied"
 	ASASoftwarePlatformName string = "asa"
+	IOSPlatformName         string = "ios"
+	IronWarePlatformName    string = "foundry"
 	JunOSPlatformName       string = "srx"
+	NXOSPlatformName        string = "nxos"
 	ScreenOSPlatformName    string = "ssg"
 	YamahaOSPlatformName    string = "yamaha"
 )
@@ -16,13 +16,13 @@ const (
 // Used for config validation
 var (
 	Platforms = []string{
-		IOSPlatformName,
-		NXOSPlatformName,
-		IronWarePlatformName,
 		AireOSPlatformName,
 		AlliedWarePlatformName,
 		ASASoftwarePlatformName,
+		IOSPlatformName,
+		IronWarePlatformName,
 		JunOSPlatformName,
+		NXOSPlatformName,
 		ScreenOSPlatformName,
 		YamahaOSPlatformName,
 	}
